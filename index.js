3c91bf2a128eb0dc4c581a668d5aefe2af01e954
Ksenia1124 <lyuksenia1124@gmail.com>
2017-05-07 22:57:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,3 +32,30 @@ app.get('/ui', function(req, res) {
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+var users = {};
+io.on('connection', function(client) {
+  client.emit('users base', users);
+  client.emit('user connected', client.id);
+  client.on('user done', function(coordx, coordy){
+    users[client.id] = {
+      x: coordx,
+      y: coordy
+    }
+    client.broadcast.emit('user done', coordx, coordy, client.id)
+  });
+ 
+  client.on('button clicked', function(value){
+    client.broadcast.emit('sprite change coord', client.id, value);
+    client.emit('button clicked', client.id, value);
+    users[client.id].x = +users[client.id].x + value
+  });
+  client.on('disconnect', function(){
+    client.broadcast.emit('user disconnected', client.id);
+    delete users[client.id];
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});

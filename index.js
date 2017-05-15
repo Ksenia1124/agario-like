0630b28466a11634fbac3781d903cae5ee100221
Ksenia1124 <lyuksenia1124@gmail.com>
2017-05-15 20:59:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,10 @@ app.get('/28', function(req, res) {
     res.sendFile(__dirname + '/Day28/index.html');
 });
 
+app.get('/29', function(req, res) {
+    res.sendFile(__dirname + '/Day29/index.html');
+});
+
 app.get('/try', function(req, res) {
     res.sendFile(__dirname + '/Day22/try.html');
 });
